004312eb340d8727e561a4a378f789381abd9cd3
zahirahmadchaudhry <zahirahmadchaudhry@gmail.com>
2022-12-06 22:15:29 +0500

    aVeryBigSum problem has been done

---
+++ Week2/Day2/Lab/aVeryBigSum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace aVeryBigSum
+{
+    internal class Program
+    {
+        static long aVeryBigSum(long[] arr)
+        {
+            long sum =0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            long[] arr = new long[long.Parse(Console.ReadLine())];
+            for(int i = 0; i<arr.Length; i++)
+            {
+                arr[i] =long.Parse(Console.ReadLine());
+            }
+            long result = aVeryBigSum(arr);
+            Console.WriteLine(result);
+        }
+    }
+}
